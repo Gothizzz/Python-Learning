bb9ae270e5b34d14b87942594ff3de0d338cac27
wangchi <whitejames@foxmail.com>
2021-09-16 22:54:02 +0800

    添加转义字符

--- 转义字符.py
+++ 转义字符.py
@@ -1,2 +1,17 @@
 # Editor: GOTHIZzz
 # Dev Date: 2021/9/16
+# 换行
+print("hello\nworld")
+# 制表
+print('hello\tworld')
+# 回退一行
+print('hello\rworld')
+# 回退一格
+print('hello\bworld')
+
+print('C:\\\\test')
+
+print('小明说：\'大家好\'')
+
+# 输出原字符，让转义字符失效
+print(r'hello\n\r\\world')
