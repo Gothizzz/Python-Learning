fbef8c0d06656766e86d3297dd70aecca1c8a43e
wangchi <whitejames@foxmail.com>
2021-09-16 23:12:58 +0800

    添加多次赋值

---
+++ 多次赋值.py
@@ -0,0 +1,18 @@
+# Editor: GOTHIZzz
+# Dev Date: 2021/9/16
+name = 'james'
+print(name)
+
+print('标识',id(name))
+print('类型',type(name))
+print('值',name)
+
+x = 1
+y = 2
+print(x+y)
+
+name='james'
+print(name)
+
+name='peter'
+print(name)
