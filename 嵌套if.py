dc55d1a9bbb1336263452437975f77789bccae39
wangchi <whitejames@foxmail.com>
2021-10-01 22:04:39 +0800

    嵌套if

--- 嵌套if.py
+++ 嵌套if.py
@@ -5,7 +5,7 @@ gender = input('当前用户的性别:')
 #判断级别
 if 0 < mystar <=20:
     if gender == 'boy':
-        mystar += 1
+        mystar += 1 # mystar = mystar +1
     else:
         mystar += 2
 print(mystar)
