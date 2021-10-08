a41ca498ce2b86aaae6ab503f9911b05b455c0c4
wangchi <whitejames@foxmail.com>
2021-10-08 22:38:27 +0800

    +

---
+++ 基础/列表的创建.py
@@ -0,0 +1,9 @@
+# Editor: GOTHIZzz
+# Dev Date: 2021/10/8
+x = ['james', 'xiongda', 56]
+
+y = list(['james', 'xiongda', 56])
+
+print(x)
+print(y)
+
---
+++ 基础/列表的特点.py
@@ -0,0 +1,10 @@
+# Editor: GOTHIZzz
+# Dev Date: 2021/10/8
+
+x = ['hello', 'james', 56, 'hello']
+
+print(x)
+
+print(x[2])
+
+print(x[-3])
