4adfbab69ac742e0bce276697a55a49439b431ee
wangchi <whitejames@foxmail.com>
2021-10-14 23:21:42 +0800

    +

--- 项目/爬取王者荣耀皮肤图片/王者荣耀头像.py
+++ 项目/爬取王者荣耀皮肤图片/王者荣耀头像.py
@@ -4,4 +4,25 @@ import requests
 from pyquery import PyQuery
 url = 'https://pvp.qq.com/web201605/herolist.shtml'
 html = requests.get(url).content
-print(html)
+# print(html)
+doc = PyQuery(html)
+# print(doc)
+items = doc('.herolist > li').items()
+
+# 循环遍历
+for item in items:
+    # 继续向下查找img标签，attr属性功能，src
+    url = item.find('img').attr('src')
+    # print(url)
+    urls = "https:"+url
+    # print(urls)
+    # name = item.find('img').attr('alt')
+    name = item.find('a').text()
+    # print(name)
+    url_content = requests.get(urls).content
+    # print(url_content)
+    # 保存数据
+    with open('./picture'+name+'.jpg','wb') as file:
+        file.write(url_content)
+        print("正在下载：%s......%s" %(name, urls))
+print("下载完毕")
