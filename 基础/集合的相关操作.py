03ed1e28eb7fd6d609f329d46fc2b097a2f08e82
wangchi <whitejames@foxmail.com>
2022-01-28 22:49:44 +0800

    集合

---
+++ 基础/集合的相关操作.py
@@ -0,0 +1,20 @@
+# Editor: GOTHIZzz
+# Dev Date: 2022/1/28
+
+s1 = {1, 2, 3, 4, 5}
+
+# 向集合里新增数据
+s1.add(6)
+print(s1)
+
+# 增加多个元素
+s1.update({100, 200, 300})
+print(s1)
+
+# 集合删除的操作
+s1.remove(100)
+print(s1)
+
+# 清空集合
+s1.clear()
+print(s1)
